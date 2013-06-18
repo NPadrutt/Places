55d8557e4ae85a84b60317fd535c43775aafb047
Nino Padrutt <nino.padrutt@hotmail.com>
2013-06-18 08:41:35 +0200

    Add wp toolkit, basic map function

--- MyTravelHistory/MyTravelHistory/Views/MapView.xaml.cs
+++ MyTravelHistory/MyTravelHistory/Views/MapView.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using System.Device.Location;
+using Microsoft.Phone.Maps.Controls;
+using Microsoft.Phone.Maps.Toolkit;
 
 namespace MyTravelHistory.Views
 {
@@ -16,6 +18,18 @@ namespace MyTravelHistory.Views
         public MapView()
         {
             InitializeComponent();
+
+            MapLayer layer = new MapLayer();
+
+            Pushpin pushpin = new Pushpin();
+
+            pushpin.GeoCoordinate = new GeoCoordinate(App.ViewModel.SelectedLocations.Latitude, App.ViewModel.SelectedLocations.Longtitude);
+            MapOverlay overlay = new MapOverlay();
+            overlay.Content = pushpin;
+            overlay.GeoCoordinate = new GeoCoordinate(App.ViewModel.SelectedLocations.Latitude, App.ViewModel.SelectedLocations.Longtitude);
+            layer.Add(overlay);
+
+            Map.Layers.Add(layer);
         }
 
         private void Map_Loaded(object sender, System.Windows.RoutedEventArgs e)
