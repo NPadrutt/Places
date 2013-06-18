757cf54e614c67049a16547a70cd3ed79723177e
Nino Padrutt <nino.padrutt@hotmail.com>
2013-06-18 16:15:17 +0200

    Add Testproject and Testframework

---
+++ MyTravelHistory/MyTravelHistoryTests/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using MyTravelHistoryTests.Resources;
+using Microsoft.Phone.Testing;
+
+namespace MyTravelHistoryTests
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.Content = UnitTestSystem.CreateTestPage();
+        }
+    }
+}
