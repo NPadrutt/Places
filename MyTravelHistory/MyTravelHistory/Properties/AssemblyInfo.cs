532627278963599da536c2f7d69953a5fb9935e0
Nino Padrutt <nino.padrutt@hotmail.com>
2013-08-07 21:44:24 +0200

    reset versi

--- MyTravelHistory/MyTravelHistory/Properties/AssemblyInfo.cs
+++ MyTravelHistory/MyTravelHistory/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Resources;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0807.9")]
+[assembly: AssemblyVersion("1.1.0.1")]
 [assembly: NeutralResourcesLanguage("en-US")]
