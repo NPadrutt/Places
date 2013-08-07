89eb81402f8c0b0ed07b46ca45b1f4ac69f62540
Nino Padrutt <nino.padrutt@hotmail.com>
2013-08-07 21:48:34 +0200

    update increment build versoin

--- MyTravelHistory/MyTravelHistory/Properties/AssemblyInfo.cs
+++ MyTravelHistory/MyTravelHistory/Properties/AssemblyInfo.cs
@@ -33,5 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.1.0.1")]
+[assembly: AssemblyVersion("1.1.0807.2")]
+[assembly: AssemblyFileVersion("1.1.0807.2")]
 [assembly: NeutralResourcesLanguage("en-US")]
