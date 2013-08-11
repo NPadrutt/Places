1015e0e9a3d6f002bc2c3869e5746307a2f6de3c
Nino Padrutt <nino.padrutt@hotmail.com>
2013-08-11 20:23:01 +0200

    add numericupdown

--- Places/Properties/AssemblyInfo.cs
+++ Places/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0811.364")]
-[assembly: AssemblyFileVersion("1.0.0811.364")]
+[assembly: AssemblyVersion("1.0.0811.370")]
+[assembly: AssemblyFileVersion("1.0.0811.370")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
