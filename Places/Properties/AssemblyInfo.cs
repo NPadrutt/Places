a31537eaaa306a9e33d1aea97131254272ae10e0
Nino Padrutt <nino.padrutt@hotmail.com>
2013-08-11 12:56:11 +0200

    update increment build version settings

--- Places/Properties/AssemblyInfo.cs
+++ Places/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0810.362")]
-[assembly: AssemblyFileVersion("1.0.0810.362")]
+[assembly: AssemblyVersion("1.0.0811.364")]
+[assembly: AssemblyFileVersion("1.0.0811.364")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
